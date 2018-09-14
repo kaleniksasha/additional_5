20a8656e8a490aa6b386e3399464a0c22c450d6e
kaleniksasha <kalenik.wr@gmail.com>
2018-09-14 23:20:09 +0300

    solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  let pairs = bracketsConfig.map(a => a[0] + a[1]);
+  let f = false;
+  do {
+    f = false;
+    for (let p of pairs) {
+      let l = str.length;
+      str = str.replace(p, "");
+      if (str.length !== l) f = true;
+    }
+  } while (f);
+  return str.length === 0;
+};
